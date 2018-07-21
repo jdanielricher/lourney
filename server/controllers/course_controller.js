615f6263ec5ed719ab6bf1380c49a10e5078a65a
jdaniel <jdricher2017@gmail.com>
2018-07-21 16:28:14 -0500

    commit

---
+++ server/controllers/course_controller.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const axios = require('axios');
+const url = "https://www.udemy.com/api-2.0/"
+let id = 0;
+
+axios({
+    method: 'get',
+    url,
+    auth: {
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD
+    }
+})
+    .then(function (response) {
+        res.send(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+const deleteCourses = (req, res, next) => {
+
+}
+
+const getCourses = (req, res, next) => {
+    console.log('hit')
+    axios.get(`${url}`).then(
+        response => {
+            course = response.data;
+            res.status(200).json(course)
+        }
+    )
+};
+
+const addCourses = (req, res, next) => {
+
+}
+
+const editCourses = (req, res, next) => {
+
+}
+
+module.exports = {
+    getCourses,
+    deleteCourses,
+    editCourses,
+    addCourses
+};
--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const course_controller = require("./controllers/course_controller");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 
-// app.get('/api-2.0/courses/', getCourses);
+app.get("/api-2.0/courses/", course_controller.getCourses)
+app.delete("/api-2.0/courses/:id", course_controller.deleteCourses)
+app.put("/api-2.0/courses/:id", course_controller.editCourses)
+app.post("/api-2.0/courses/", course_controller.addCourses)
+
 
 const port = process.env.SERVER_PORT || 3001;
 app.listen(port, () => { console.log(`Listening on port: ${port}`); });
